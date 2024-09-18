0299d05be0f13da11e8365b709d659f68e0dc4da
harshraj070 <146359469+harshraj070@users.noreply.github.com>
2024-09-18 17:21:24 +0530

    Create ERC-721.sol

---
+++ ERC-721.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Web3Builder is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
+    uint256 private _nextTokenId;
+    uint256 public maxSupply = 2;
+
+    constructor(address initialOwner)
+        ERC721("Web3Builder", "WE3")
+        Ownable(initialOwner)
+    {}
+    bool public publicMintOpen = false;
+    bool public allowListMintOpen = false;
+
+    mapping(address=>bool)public allowList;
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmY5rPqGTN1rZxMQg2ApiSZc7JiBNs1ryDzXPZpQhC1ibm/";
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function editMintWindows(
+        bool _publicMintOpen,
+        bool _allowListMintOpen
+    ) external onlyOwner {
+        publicMintOpen = _publicMintOpen;
+        allowListMintOpen = _allowListMintOpen;
+    }
+
+    function allowListMint() public payable{
+        require(allowListMintOpen, " AllowList Mint closed");
+        require(allowList[msg.sender],"Not allowed");
+        require(msg.value == 0.0001 ether, 'Min limit not satisfied.');
+         require(_nextTokenId < maxSupply,"Max limit reached");
+         uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function publicMint() public payable{
+        require(publicMintOpen, "Public Mint closed");
+        require(msg.value == 0.001 ether,"Value not sufficient");
+        require(_nextTokenId < maxSupply,"Max limit reached");
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function setAllowList(address[] calldata addresses) external onlyOwner {
+        for(uint256 i = 0; i< addresses.length; i++){
+            allowList[addresses[i]] = true;
+        }
+    }
+
+    function withdraw(address _addr) external onlyOwner{
+        uint256 balance = address(this).balance;
+        payable(_addr).transfer(balance);
+    }
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
