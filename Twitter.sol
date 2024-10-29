039c85a80825f4c1f3bf11c812bffdc5545ce80a
Harsh Raj <146359469+harshraj070@users.noreply.github.com>
2024-10-29 20:02:12 +0530

    Update Twitter.sol

--- Twitter.sol
+++ Twitter.sol
@@ -1,31 +1,36 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-contract Twitter{
+
+contract Twitter {
 
     uint256 public MAX_len = 280;
     address public owner;
 
-    event TweetCreated(uint256 id, address author, string content,uint256 timestamp);
+    event TweetCreated(uint256 id, address author, string content, uint256 timestamp);
     event TweetLiked(address liker, address tweetAuthor, uint256 tweetId, uint256 newLikeCount);
     event TweetUnliked(address unliker, address tweetAuthor, uint256 tweetId, uint256 newLikeCount);
-    struct tweet{
+
+    struct tweet {
         uint256 id;
         address author;
         string content;
         uint256 likes;
         uint256 timestamp;
     }
-    constructor(){
+
+    constructor() {
         owner = msg.sender;
     }
-    modifier onlyOwner(){
-        require(msg.sender == owner,"Only the owner is allowed");
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner is allowed");
         _;
     }
 
-    mapping(address => tweet[])public tweets;
-    function createTweet(string memory content_)public{
-        require(bytes(content_).length <= MAX_len,"Tweet is too long");
+    mapping(address => tweet[]) public tweets;
+
+    function createTweet(string memory content_) public {
+        require(bytes(content_).length <= MAX_len, "Tweet is too long");
         tweet memory newtweet = tweet({
             id: tweets[msg.sender].length,
             author: msg.sender,
@@ -39,38 +44,43 @@ contract Twitter{
         emit TweetCreated(newtweet.id, newtweet.author, newtweet.content, newtweet.timestamp);
     }
 
-    function liketweet(address author, uint256 id)external{
-        require(tweets[author][id].id == id,"Tweet does not exist");
-        tweets[author][id].likes +=1;
+    function likeTweet(address author, uint256 id) external {
+        require(tweets[author][id].id == id, "Tweet does not exist");
+        tweets[author][id].likes += 1;
 
         emit TweetLiked(msg.sender, author, id, tweets[author][id].likes);
     }
-    function unlike(address author, uint256 id)external{
-        require(tweets[author][id].id == id,"Tweet does not exist");
-        require(tweets[author][id].likes > 0,"The number of likes cannot be less than 0");
-        tweets[author][id].likes -=1;
+
+    function unlikeTweet(address author, uint256 id) external {
+        require(tweets[author][id].id == id, "Tweet does not exist");
+        require(tweets[author][id].likes > 0, "The number of likes cannot be less than 0");
+        tweets[author][id].likes -= 1;
 
         emit TweetUnliked(msg.sender, author, id, tweets[author][id].likes);
     }
 
-    function changtweetlength(uint256 newlength)public onlyOwner{
-        MAX_len = newlength;
+    function changeTweetLength(uint256 newLength) public onlyOwner {
+        MAX_len = newLength;
     }
 
-    function getTotalLikes(address _author) external view returns(uint256){
-        uint255 totalLikes;
-        for(uint i =0;i< tweets[_author].length; i++){
+    function getTotalLikes(address _author) external view returns (uint256) {
+        uint256 totalLikes;
+        for (uint256 i = 0; i < tweets[_author].length; i++) {
             totalLikes += tweets[_author][i].likes;
         }
-
         return totalLikes;
     }
 
-    function getTweet(uint256 i)public view returns(tweet memory){
+    function getTweet(uint256 i) public view returns (tweet memory) {
         return tweets[msg.sender][i];
     }
-    
-    function getallTweets(address owner_)public view returns(tweet[] memory){
+
+    function getAllTweets(address owner_) public view returns (tweet[] memory) {
         return tweets[owner_];
+    }
+
+    function deleteTweet(address author, uint256 id) external onlyOwner {
+        require(tweets[author][id].id == id, "Tweet does not exist");
+        delete tweets[author][id];
     }
 }
