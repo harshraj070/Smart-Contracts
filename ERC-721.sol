f0febdd5085860e8f8d227141a94837a2c7c91de
Harsh Raj <146359469+harshraj070@users.noreply.github.com>
2024-10-30 19:17:32 +0530

    Update ERC-721.sol

--- ERC-721.sol
+++ ERC-721.sol
@@ -15,10 +15,11 @@ contract Web3Builder is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
         ERC721("Web3Builder", "WE3")
         Ownable(initialOwner)
     {}
+
     bool public publicMintOpen = false;
     bool public allowListMintOpen = false;
 
-    mapping(address=>bool)public allowList;
+    mapping(address => bool) public allowList;
 
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://QmY5rPqGTN1rZxMQg2ApiSZc7JiBNs1ryDzXPZpQhC1ibm/";
@@ -40,32 +41,37 @@ contract Web3Builder is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
         allowListMintOpen = _allowListMintOpen;
     }
 
-    function allowListMint() public payable{
-        require(allowListMintOpen, " AllowList Mint closed");
-        require(allowList[msg.sender],"Not allowed");
-        require(msg.value == 0.0001 ether, 'Min limit not satisfied.');
-         require(_nextTokenId < maxSupply,"Max limit reached");
-         uint256 tokenId = _nextTokenId++;
+    function allowListMint() public payable {
+        require(allowListMintOpen, "AllowList Mint closed");
+        require(allowList[msg.sender], "Not allowed");
+        require(msg.value == 0.0001 ether, "Min limit not satisfied.");
+        require(_nextTokenId < maxSupply, "Max limit reached");
+        uint256 tokenId = _nextTokenId++;
         _safeMint(msg.sender, tokenId);
     }
 
-    function publicMint() public payable{
+    function publicMint() public payable {
         require(publicMintOpen, "Public Mint closed");
-        require(msg.value == 0.001 ether,"Value not sufficient");
-        require(_nextTokenId < maxSupply,"Max limit reached");
+        require(msg.value == 0.001 ether, "Value not sufficient");
+        require(_nextTokenId < maxSupply, "Max limit reached");
         uint256 tokenId = _nextTokenId++;
         _safeMint(msg.sender, tokenId);
     }
 
     function setAllowList(address[] calldata addresses) external onlyOwner {
-        for(uint256 i = 0; i< addresses.length; i++){
+        for (uint256 i = 0; i < addresses.length; i++) {
             allowList[addresses[i]] = true;
         }
     }
 
-    function withdraw(address _addr) external onlyOwner{
+    function withdraw(address _addr) external onlyOwner {
         uint256 balance = address(this).balance;
         payable(_addr).transfer(balance);
+    }
+
+    function deleteToken(uint256 tokenId) external onlyOwner {
+        require(_exists(tokenId), "Token does not exist");
+        _burn(tokenId);
     }
 
     function _update(address to, uint256 tokenId, address auth)
