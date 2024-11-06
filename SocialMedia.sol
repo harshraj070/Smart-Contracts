e9cdd6d69191d611f089410e1516a049b105933b
Harsh Raj <146359469+harshraj070@users.noreply.github.com>
2024-11-06 20:02:30 +0530

    Update SocialMedia.sol

--- SocialMedia.sol
+++ SocialMedia.sol
@@ -12,13 +12,11 @@ contract SocialMedia {
 
     mapping(address => Profile) public profiles;
 
-    // Set or update the user's profile
     function setProfile(string memory _username, string memory _bio) public {
         profiles[msg.sender].username = _username;
         profiles[msg.sender].bio = _bio;
     }
 
-    // Follow another user
     function follow(address _user) public {
         require(_user != msg.sender, "You cannot follow yourself");
         require(!profiles[_user].following[msg.sender], "You are already following this user");
@@ -27,7 +25,6 @@ contract SocialMedia {
         profiles[_user].following[msg.sender] = true;
     }
 
-    // Unfollow a user
     function unfollow(address _user) public {
         require(_user != msg.sender, "You cannot unfollow yourself");
         require(profiles[_user].following[msg.sender], "You are not following this user");
@@ -36,23 +33,19 @@ contract SocialMedia {
         profiles[_user].following[msg.sender] = false;
     }
 
-    // Update only the bio of the profile
     function updateBio(string memory _bio) public {
         profiles[msg.sender].bio = _bio;
     }
 
-    // Check if a user is following another user
     function isFollowing(address _user, address _follower) public view returns (bool) {
         return profiles[_user].following[_follower];
     }
 
-    // Get the profile details of a user
     function getProfile(address _user) public view returns (string memory, string memory, uint) {
         Profile storage profile = profiles[_user];
         return (profile.username, profile.bio, profile.followersCount);
     }
 
-    // Simple private messaging (DM) system
     mapping(address => mapping(address => string[])) private messages;
 
     function sendMessage(address _recipient, string memory _message) public {
@@ -62,5 +55,15 @@ contract SocialMedia {
 
     function getMessages(address _sender) public view returns (string[] memory) {
         return messages[msg.sender][_sender];
+    }
+
+    function deleteMessage(address _sender, uint256 index) public {
+        require(index < messages[msg.sender][_sender].length, "Invalid message index");
+
+        string[] storage userMessages = messages[msg.sender][_sender];
+        for (uint i = index; i < userMessages.length - 1; i++) {
+            userMessages[i] = userMessages[i + 1];
+        }
+        userMessages.pop();
     }
 }
